089296182c2efc2628027a587dc129c3ca35e4ff
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-08-11 21:11:19 -0300

    chore: add jest config to mongodb

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  watchPathIgnorePatterns: ['globalConfig']
 }
