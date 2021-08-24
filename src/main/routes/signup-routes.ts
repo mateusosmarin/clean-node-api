db2232ec4b1fa6ddeab8d6c7c0c8058c034fdc0d
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-08-24 00:17:07 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/signup'
 
-export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: true })
-  })
+export default async (router: Router): Promise<void> => {
+  const signUpController = makeSignUpController()
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.post('/signup', adaptRoute(signUpController))
 }
