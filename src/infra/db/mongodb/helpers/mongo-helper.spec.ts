fd323c43b9604d2334e0391cf6c5b56bedf250d2
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-08-24 00:42:16 -0300

    test: ensure mongodb reconnects if connection is lost

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,23 @@
+import { MongoHelper as sut } from './mongo-helper'
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    if (process.env.MONGO_URL) {
+      await sut.connect(process.env.MONGO_URL)
+    } else {
+      throw new Error('MONGO_URL is not set')
+    }
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('Should reconnect if mongodb is down', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -16,7 +16,7 @@ describe('SignUp Route', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
