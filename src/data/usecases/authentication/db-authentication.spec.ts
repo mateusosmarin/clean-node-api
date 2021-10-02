54b2d9433d9ef9f61dd00ba4e8611d92321b5b0e
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-10-02 18:11:47 -0300

    test: ensure DbAuthentication returns null if HashComparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -94,4 +94,11 @@ describe('DbAuthentication', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if HashComparer returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockResolvedValueOnce(false)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
