b368a9f9568faa30e7692cd4fc8afaf3f1209aa2
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-10-02 15:30:21 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -41,4 +41,10 @@ describe('ValidationComposite', () => {
     const error = sut.validate({})
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeUndefined()
+  })
 })
