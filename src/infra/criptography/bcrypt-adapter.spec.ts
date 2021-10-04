ed83e0edd7603f3c9f59f28d0b042c6e9c14af48
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-10-04 19:48:09 -0300

    test: ensure BcryptAdapter returns false if compare fails

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -52,4 +52,14 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any value', 'any_hash')
     expect(isValid).toBe(true)
   })
+
+  test('Should return false when compare fails', async () => {
+    const sut = makeSut()
+    jest
+      .spyOn(bcrypt, 'compare')
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      .mockImplementationOnce(async () => false)
+    const isValid = await sut.compare('any value', 'any_value')
+    expect(isValid).toBe(false)
+  })
 })
