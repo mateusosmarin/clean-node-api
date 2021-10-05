55cf17a52272332b8f995c317e4c8989cd14ced7
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-10-04 21:00:04 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -53,11 +53,28 @@ describe('Account Mongo Repository', () => {
     expect(account.name).toBe('any name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any password')
+    expect(account.accessToken).toBeFalsy()
   })
 
   test('Should return null if loadByEmail fails', async () => {
     const sut = makeSut()
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
+  })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const result = await accountCollection.insertOne({
+      name: 'any name',
+      email: 'any_email@mail.com',
+      password: 'any password'
+    })
+    const fakeAccount = result.ops[0]
+    await sut.updateAccessToken(fakeAccount._id, 'any_token')
+    const account = await accountCollection.findOne({
+      _id: fakeAccount._id
+    })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
   })
 })
