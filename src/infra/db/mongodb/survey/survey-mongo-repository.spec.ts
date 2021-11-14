2984b2e1650c91a6aaceede2baaacf82230dc024
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-11-14 12:58:54 -0300

    test: ensure SurveyMongoRepository adds a survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,48 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
+describe('Survey Mongo Repository', () => {
+  let surveyCollection: Collection
+
+  beforeAll(async () => {
+    if (process.env.MONGO_URL) {
+      await MongoHelper.connect(process.env.MONGO_URL)
+    } else {
+      throw new Error('MONGO_URL not set')
+    }
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  test('Should add a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'another_answer'
+        }
+      ]
+    })
+    const survey = await surveyCollection.findOne({
+      question: 'any_question'
+    })
+    expect(survey).toBeTruthy()
+  })
+})
