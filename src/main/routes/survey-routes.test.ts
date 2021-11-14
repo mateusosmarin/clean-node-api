d5be2a8a3136aab0359741fee7028137f5c9a907
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-11-14 13:25:41 -0300

    test: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,46 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('Survey Routes', () => {
+  let surveyCollection: Collection
+
+  beforeAll(async () => {
+    if (process.env.MONGO_URL) {
+      await MongoHelper.connect(process.env.MONGO_URL)
+    } else {
+      throw new Error('MONGO_URL not set')
+    }
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2',
+              image: 'http://image-name.com'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
