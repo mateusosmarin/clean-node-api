eff86de70454bd777f29007ceaf84442f985cab1
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-11-14 02:53:08 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,2 +1,2 @@
-export * from '../../../../domain/usecases/authentication'
+export * from '../../../../domain/usecases/add-survey'
 export * from '../../../protocols'
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,18 +1,28 @@
 import { badRequest } from '../../../helpers/http/http-helper'
 import { Validation } from '../../../protocols'
 import {
+  AddSurvey,
   Controller,
   HttpRequest,
   HttpResponse
 } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error != null) {
       return badRequest(error)
     }
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      question,
+      answers
+    })
     return {
       statusCode: 200,
       body: {}
