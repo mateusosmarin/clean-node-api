53dba9462d3d649944609aaf63a86adb3f99e850
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2022-05-16 18:32:58 -0300

    refactor: move access token creation to a factory method

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -9,6 +9,28 @@ describe('Survey Routes', () => {
   let surveyCollection: Collection
   let accountCollection: Collection
 
+  const makeAccessToken = async (): Promise<string> => {
+    const res = await accountCollection.insertOne({
+      name: 'Mateus',
+      email: 'mateus.osmarin@gmail.com',
+      password: '123456',
+      role: 'admin'
+    })
+    const id = res.ops[0]._id
+    const accessToken = jwt.sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          accessToken
+        }
+      }
+    )
+    return accessToken
+  }
+
   beforeAll(async () => {
     if (process.env.MONGO_URL) {
       await MongoHelper.connect(process.env.MONGO_URL)
@@ -50,24 +72,7 @@ describe('Survey Routes', () => {
     })
 
     test('Should return 204 on add survey with valid access token', async () => {
-      const res = await accountCollection.insertOne({
-        name: 'Mateus',
-        email: 'mateus.osmarin@gmail.com',
-        password: '123456',
-        role: 'admin'
-      })
-      const id = res.ops[0]._id
-      const accessToken = jwt.sign({ id }, env.jwtSecret)
-      await accountCollection.updateOne(
-        {
-          _id: id
-        },
-        {
-          $set: {
-            accessToken
-          }
-        }
-      )
+      const accessToken = await makeAccessToken()
       await request(app)
         .post('/api/surveys')
         .set('x-access-token', accessToken)
@@ -94,38 +99,12 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
-    test('Should return 200 on load surveys with valid access token', async () => {
-      const res = await accountCollection.insertOne({
-        name: 'Mateus',
-        email: 'mateus.osmarin@gmail.com',
-        password: '123456'
-      })
-      const id = res.ops[0]._id
-      const accessToken = jwt.sign({ id }, env.jwtSecret)
-      await accountCollection.updateOne(
-        {
-          _id: id
-        },
-        {
-          $set: {
-            accessToken
-          }
-        }
-      )
-      await surveyCollection.insertMany([{
-        question: 'any_question',
-        answers: [
-          {
-            image: 'any_image',
-            answer: 'any_answer'
-          }
-        ],
-        date: new Date()
-      }])
+    test('Should return 204 on load surveys with valid access token', async () => {
+      const accessToken = await makeAccessToken()
       await request(app)
         .get('/api/surveys')
         .set('x-access-token', accessToken)
-        .expect(200)
+        .expect(204)
     })
   })
 })
