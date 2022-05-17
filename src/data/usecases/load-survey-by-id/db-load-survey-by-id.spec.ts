02aa673b9dbe85bbbfcab7b869016b19616a51c3
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2022-05-17 00:09:09 -0300

    test: ensure DbLoadSurveyById returns a survey if LoadSurveyByIdRepository succeeds

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -55,4 +55,10 @@ describe('DbLoadSurveys', () => {
     await sut.loadById('any_id')
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('should return a survey on success', async () => {
+    const { sut } = makeSUT()
+    const surveys = await sut.loadById('any_id')
+    expect(surveys).toEqual(makeFakeSurvey())
+  })
 })
