b7c2704fb0dd3f1125e0c94fec6ca19ab5ac2bd5
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2022-05-17 01:48:48 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if there is no data with accountId and surveyId

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,23 @@
+import { SaveSurveyResultRepository } from '@data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { SurveyResultModel } from '@domain/models/survey-result'
+import { SaveSurveyResultModel } from '@domain/usecases/save-survey-result'
+import { mongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (surveyResult: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await mongoHelper.getCollection('surveyResults')
+    const res = await surveyResultCollection.findOneAndUpdate({
+      surveyId: surveyResult.surveyId,
+      accountId: surveyResult.accountId
+    }, {
+      $set: {
+        answer: surveyResult.answer,
+        date: surveyResult.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+    return res.value && mongoHelper.map(res.value)
+  }
+}
