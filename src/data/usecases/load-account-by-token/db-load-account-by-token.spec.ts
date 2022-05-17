bb606d616c02dc08a09b6e902d3acee470ff53b8
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2022-05-17 00:30:52 -0300

    refactor: add data usecases protocol files to simplify imports

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,7 +1,9 @@
-import { Decrypter } from '@data/protocols/cryptography/decrypter'
-import { LoadAccountByTokenRepository } from '@data/protocols/db/account/load-account-by-token-repository'
-import { AccountModel } from '@data/usecases/add-account/db-add-account-protocols'
 import { DbLoadAccountByToken } from './db-load-account-by-token'
+import {
+  AccountModel,
+  Decrypter,
+  LoadAccountByTokenRepository
+} from './db-load-account-by-token.protocols'
 
 type SUTTypes = {
   sut: DbLoadAccountByToken
