1f8b2821444ac8129900067b14001336c3bf0354
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2022-05-19 22:18:53 -0300

    feat: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -34,13 +34,13 @@ export class SaveSurveyResultController implements Controller {
       if (!accountId) {
         return forbidden(new InvalidParamError('accountId'))
       }
-      await this.saveSurveyResult.save({
+      const surveyResult = await this.saveSurveyResult.save({
         accountId,
         surveyId,
         answer,
         date: new Date()
       })
-      return ok({})
+      return ok(surveyResult)
     } catch (error) {
       return serverError(error)
     }
