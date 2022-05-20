411eb9098e0d74cffe2b063281922515fd9c4c34
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2022-05-19 22:34:59 -0300

    test: ensure save survey result route returns 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,28 @@
+import { mongoHelper } from '@infra/db/mongodb/helpers/mongo-helper'
+import app from '@main/config/app'
+import request from 'supertest'
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    if (process.env.MONGO_URL) {
+      await mongoHelper.connect(process.env.MONGO_URL)
+    } else {
+      throw new Error('MONGO_URL not set')
+    }
+  })
+
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without access token', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
