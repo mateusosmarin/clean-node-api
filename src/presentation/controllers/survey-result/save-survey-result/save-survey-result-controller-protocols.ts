a70f1aeaa1a9ad622bfaa2c00f6a9d19840e7cb2
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2022-05-19 22:07:00 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -1,4 +1,6 @@
 export * from '@domain/models/survey'
+export * from '@domain/models/survey-result'
+export * from '@domain/usecases/survey-result/save-survey-result'
 export * from '@domain/usecases/survey/load-survey-by-id'
 export * from '@presentation/helpers/http/http-helper'
 export * from '@presentation/protocols'
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,16 +5,21 @@ import {
   HttpRequest,
   HttpResponse,
   LoadSurveyById,
+  SaveSurveyResult,
   ok
 } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
+      const { accountId } = httpRequest
       const survey = await this.loadSurveyById.loadById(surveyId)
       if (survey) {
         const answers = survey.answers.map(
@@ -26,6 +31,15 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      if (!accountId) {
+        return forbidden(new InvalidParamError('accountId'))
+      }
+      await this.saveSurveyResult.save({
+        accountId,
+        surveyId,
+        answer,
+        date: new Date()
+      })
       return ok({})
     } catch (error) {
       return serverError(error)
