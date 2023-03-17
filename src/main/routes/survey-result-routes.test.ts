0fe04c38c76d49320bae55197532822576875e0c
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2023-03-16 21:30:55 -0300

    test: ensure load survey result route returns 403 if no access token is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 
-describe('Survey Routes', () => {
+describe('Survey Result Routes', () => {
   let surveyCollection: Collection
   let accountCollection: Collection
 
@@ -84,6 +84,12 @@ describe('Survey Routes', () => {
           answer: 'Answer 1'
         })
         .expect(200)
+    })
+  })
+
+  describe('GET /surveys/:surveyId/results', () => {
+    test('Should return 403 on load survey result without access token', async () => {
+      await request(app).get('/api/surveys/any_id/results').expect(403)
     })
   })
 })
